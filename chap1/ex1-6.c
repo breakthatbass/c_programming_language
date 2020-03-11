1fd8d18778a5f9e7417f43b795b8dabae0c300b8
breakthatbass <gamache.taylor@gmail.com>
2020-03-11 08:14:46 -0400

    fix typos in comments of both files

--- chap1/ex1-6.c
+++ chap1/ex1-6.c
@@ -1,6 +1,6 @@
 /*
-  Exercise 1-7
-  Write a program to print the value of EOF
+  Exercise 1-6
+  Verify that the expression getchar() != EOF is 0 or 1
 */
 
 #include <stdio.h>
--- chap1/ex1-7.c
+++ chap1/ex1-7.c
@@ -1,6 +1,6 @@
 /*
-  Exercise 1 - 6
-  Verify that the expression getchar() != EOF is 0 or 1
+  Exercise 1-7
+  Write a program to print the value of EOF
 */
 
 #include <stdio.h>
