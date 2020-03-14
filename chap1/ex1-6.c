191db4841d24e91c076d0eb1b517b6e1c673117b
breakthatbass <gamache.taylor@gmail.com>
2020-03-14 14:09:57 -0400

    update exercise 1-6

--- chap1/ex1-6.c
+++ chap1/ex1-6.c
@@ -9,13 +9,12 @@ int main()
 {
   int c;
 
-  while ((c = getchar()) != EOF)
-    if (EOF == 1 || EOF == 0)
-    {
-      printf("EOF = 1 or 0\n");
-    }
-    else
-    {
-      putchar(c);
-    }
+  c = getchar() != EOF;
+  
+  if (c == 1 || c == 0)
+  {
+    putchar(c);
+    printf("%d\n", c);
+  }
+  return 0;
 }
