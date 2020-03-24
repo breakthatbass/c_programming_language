94029ae2518d2fbc1560bac4df82af746a6f9129
breakthatbass <gamache.taylor@gmail.com>
2020-03-24 07:31:41 -0400

    add exercise 1-15

---
+++ chap1/ex1-15.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void temp();
+
+int main()
+{
+    temp();
+}
+
+void temp()
+{
+    float fahr, celsius;
+    int lower, upper, step;
+
+    lower = 0;
+    upper = 300;
+    step = 20;
+
+    fahr = lower;
+    while (fahr <= upper)
+    {
+        celsius = (5.0/9.0) * (fahr - 32.0);
+        printf("%3.0f \t %6.1f\n", fahr, celsius);
+        fahr = fahr + step;
+    }
+}
