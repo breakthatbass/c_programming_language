bb54f2ca32d369dcfdcf290caa2817cedb8163d4
breakthatbass <gamache.taylor@gmail.com>
2020-04-01 08:08:25 -0400

    add exercise 1-22

---
+++ chap1/1-22.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#define N 20    /* column to move to next line */
+
+int main () {
+
+  int c, i;
+
+  i = 0;
+// get input with getchar()
+  while ((c = getchar()) != EOF)
+  {
+    if (i == N - 1)
+    {
+      putchar(c);
+      putchar('\n');
+    }
+    else {
+      putchar(c);
+    }
+    i++;
+    // if i reaches N, reset to 0 to be able to fold very long lines
+    if (i == N) {
+      i = 0;
+    }
+  }
+}
