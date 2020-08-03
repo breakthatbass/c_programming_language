f5ffab91a3d2018ab58dbb20f0d8e5f0d0c64ec0
breakthatbass <gamache.taylor@gmail.com>
2020-08-03 17:09:13 -0400

    add buggy strncat

--- chap5/5-5.c
+++ chap5/5-5.c
@@ -33,6 +33,30 @@ void *strncopy(char *s, char *t, int n)
     return s;
 }
 
+/* concatinate at most n characters of string t to string s,
+ * terminate s with '\0'; return s */
+
+// BUGGY
+char *strncatt(char *s, char *t, int n)
+{
+    int i = 0;
+    // go to end of t
+    while (i < strlen(s))
+        *s++;
+
+    i = 0;
+    while ((*s++ = *t++) && i < n)
+        i++;
+
+    *s++ = '\0';
+    return s;
+}
+
+/* compare at most n characters of string s to string t;
+ * return < 0 if s<t, 0 if s == t, or > 0 if s>t. */
+// int strncmp(char *s, char *t, int n)
+
+
 int main()
 {
     char *str1 = "hello";
@@ -40,7 +64,16 @@ int main()
 
     strncopy(str2, str1, 2);
 
-    printf("%s\n", str2);
+    printf("strncpy: %s\n", str2);
+
+    char h[MAXLEN] = "hello";
+    char w[MAXLEN] = " world";
+
+    printf("%d\n", strlen(h));
+    
+    strncatt(h, w, 2);
+
+    printf("strncat: %s\n", w);
 
     return 0;
 }
