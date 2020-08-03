7136af72342c710aad6927bd400a1b757738bbf3
breakthatbass <gamache.taylor@gmail.com>
2020-08-03 14:16:39 -0400

    add 5-5 exercise - first part

---
+++ chap5/5-5.c
@@ -0,0 +1,43 @@
+/*
+ * 
+ * Exercise 5-5: Write versions of the library functions strncpy, strncat, and strncmp,
+ * which operate on at most the first n characters of their argument strings.
+ * For example, strncpy(s,t,n) copies at most n characters of t to s.
+ * Full descriptions are in Appendix B.
+ * 
+ */
+#include <stdio.h>
+#include <string.h>
+
+/* copy at most n characters of string t to s; return s.
+ * pad with '\0's if t has fewer than n characters. */
+void strncopy(char *s, char *t, int n)
+{
+    int t_len = strlen(t);
+    int i;
+
+    if (t_len <= n) {
+        while ((*s++ = *t++)) 
+            ;
+    } else {
+        i = 1;
+        while((*s++ = *t++) && i < n) {
+            i++;
+        }
+        for ( ; i <= n; i++) {
+            *s++ = '\0';
+        }
+    }
+}
+
+int main()
+{
+    char *str1 = "hello";
+    char str2[10];
+
+    strncopy(str2, str1, 4);
+
+    printf("%s\n", str2);
+
+    return 0;
+}
