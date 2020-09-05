9e90928d697d4b087a6b5125ce5002ec378427e8
breakthatbass <gamache.taylor@gmail.com>
2020-09-05 19:52:01 -0400

    add directions

--- chap7/7-7.c
+++ chap7/7-7.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+/* *
+ * Exercise 7-7: Modify the pattern finding program of Chapter 5
+ * to take its input from a set of named files or, if no files are
+ * named as arguments, from the standard input.
+ * */
+
 #include <string.h>
 #include <stdlib.h>
 
