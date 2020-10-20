61fedb52f36d67cd54b385ca611e5e1afb9ab1c0
breakthatbass <gamache.taylor@gmail.com>
2020-10-20 12:22:15 -0400

    update exercise 1-7

--- chap1/1-7.c
+++ chap1/1-7.c
@@ -7,15 +7,6 @@
 
 int main()
 {
-  int c;
-
-  c = getchar() != EOF;
-
-  printf("%i\n", EOF);
-
-  if (c == 1 || c == 0)
-  {
-    printf("%d\n", c);
-  }
-  return 0;
+	printf("EOF is: %d\n", EOF);
+	return 0;
 }
