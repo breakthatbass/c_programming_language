2aebc385ead48e1376e0da2a520d707ee82f1ec5
breakthatbass <gamache.taylor@gmail.com>
2020-10-20 15:53:46 -0400

    refactor exercise 1-12

--- chap1/1-12.c
+++ chap1/1-12.c
@@ -8,15 +8,27 @@
 
 #include <stdio.h>
 
+#define IN 1
+#define OUT 0
+
 int main ()
 {
-  int c;
+  int c, state;
+  state = OUT;
 
   while ((c = getchar()) != EOF)
   {
-    if (c == ' ' || c == '\t') {
-      c = '\n';
+    if (c == ' ' || c == '\n' || c == '\t') {
+			if (state == IN) {  // reached the end of word
+        putchar('\n');
+        state = OUT;
+      }
     }
-    putchar(c);
+    else if (state == OUT) {
+      state = IN;
+      putchar(c);
+    } else
+      putchar(c);
   }
+  return 0;
 }
--- chap1/1-8.c
+++ chap1/1-8.c
@@ -1,34 +1,31 @@
-/******************************************************************************
-
-created by Taylor Gamache
-Mar 19, 2020
-
-exercise 1-8
-write a program to count blanks, tabs, and newlines
-
-******************************************************************************/
+/*
+* exercise 1-8: write a program to count blanks, tabs, and newlines
+*
+*/
 
 #include <stdio.h>
 
 int main()
 {
-  int c;
-  int blanks, nl, tabs = 0;
+	int c;
+	unsigned int blanks, tabs, nlines;
+	
+  // these need to be defined separately otherwise junk values are produced
+	blanks = 0;
+	tabs = 0;
+	nlines = 0;	
+  
+	while ((c = getchar()) != EOF) {
+		if (c == ' ')
+			blanks++;
+		if (c == '\t')
+			tabs++;
+		if (c == '\n')
+			nlines++;		
+	}
+	printf("blanks: %d\n", blanks);
+	printf("tabs: %d\n", tabs);
+	printf("new lines: %d\n", nlines);
 
-  while ((c = getchar()) != EOF)
-  {
-    // check for new lines
-    if (c == '\n'){
-      nl++;
-    }
-    if (c == ' ')
-    {
-      blanks++;
-    }
-    if (c == '\t')
-    {
-      tabs++;
-    }
-  }
-  printf("blanks: %d\nnew lines: %d\ntabs: %d\n", blanks, nl, tabs);
+	return 0;
 }
